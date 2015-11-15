6d10297ad07fdec423adeab73b84c610ab000185
hexophile <hexophile@gmail.com>
2015-11-15 15:46:09 +0100

    lol

---
+++ PP/kartkówka2.pas
@@ -0,0 +1,5 @@
+uses sysutils;
+begin
+  writeln( ( (StrToInt(Paramstr(1))+StrToInt(Paramstr(2)) ) * ( StrToInt(Paramstr(2)) - StrToInt(Paramstr(1)) + 1 ) ) div 2 );
+  readln;  // Program ma wyświetlić sumę kolejnych liczb całkowitych na podanym przedziale
+end.
