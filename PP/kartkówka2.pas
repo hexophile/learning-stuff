0f3aeb551e3694c92dbf29f53c5c9bd2e2f88cd1
hexophile <hexophile@gmail.com>
2015-11-15 15:51:26 +0100

    Update kartkówka2.pas

--- PP/kartkówka2.pas
+++ PP/kartkówka2.pas
@@ -1,5 +1,5 @@
 uses sysutils;
 begin
   writeln( ( (StrToInt(Paramstr(1))+StrToInt(Paramstr(2)) ) * ( StrToInt(Paramstr(2)) - StrToInt(Paramstr(1)) + 1 ) ) div 2 );
-  readln;  // Program ma wyświetlić sumę kolejnych liczb całkowitych na podanym przedziale
+  readln;  // Program ma wyświetlić sumę kolejnych liczb całkowitych na podanym przedziale (przedział ma być rosnący)
 end.
