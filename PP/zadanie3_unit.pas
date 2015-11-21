0077025858c5023a7a4afa3b63a39eb16822b8f3
hexophile <hexophile@gmail.com>
2015-11-21 15:59:57 +0100

    Update zadanie3_unit.pas
    I changed return type for the 'FindObject' function from TPerson to TPerson pointer.
    I did that for two reasons:
    1. I wanted to make function be able to return a nil value.
    2. For the future, pointers are making database management more flexible.

--- PP/zadanie3_unit.pas
+++ PP/zadanie3_unit.pas
@@ -6,8 +6,8 @@ uses crt, sysutils;
  
 // Constants
 const
-     LOW = 0;      // Starting number in database
-     HIGH = 100;   // Size of database
+     LOW = 1;      // Starting number in database
+     HIGH = 1001;   // Ending number
 
 // Structures
 type
@@ -15,12 +15,14 @@ type
             id:integer;     // ID number
             name:string;    // Main information
             state:boolean;  // Infomraiton whether record exists
-    End; // Main database object structure
-    
+    End; // Main database object structure   
+    TPersonPtr = ^TPerson;
+
     TDatabase = array[LOW..HIGH-1] of TPerson;
-// Functions
-function FindObject( var Database:array of TPerson; tabID:integer ):TPerson;
-function ObjectExists( var Database:array of TPerson; tabID:integer ):boolean;
+
+// Functions                                                                 
+function ObjectExists( var Database:array of TPerson; dbObject:TPerson ):boolean;
+function FindObject( var Database:array of TPerson; name:string ):TPersonPtr;
 function ModifyObject( obj:TPerson ):TPerson;
 
 // Procedures
@@ -32,15 +34,34 @@ procedure SortDatabase( var Database:array of TPerson; order:boolean; by:byte );
 
 implementation
 { Functions }
-function FindObject( var Database:array of TPerson; tabID:integer ):TPerson; // Function has to find record and return it
+function ObjectExists( var Database:array of TPerson; dbObject:TPerson ):boolean; // Function returns if object exists
 Begin
-     FindObject := Database[tabID];
-End; { End of FindObject }
-
-function ObjectExists( var Database:array of TPerson; tabID:integer ):boolean; // Function returns if object exists
-Begin
-     ObjectExists := Database[tabID].state;
+     ObjectExists := dbObject.state;
 End; { End of ObjectExists }
+
+function FindObject( var Database:array of TPerson; name:string ):TPersonPtr; // Function has to find record and return pointer to it
+var
+   i:integer;
+Begin
+     i := LOW;
+
+     while( not ( Database[i].name = name ) ) do // Pretty primitive, but working. TODO for extended
+     begin
+          if( HIGH-1 = i ) then
+          begin
+               FindObject := nil;
+               exit;
+          end;
+
+          i := i + 1;
+     end;
+
+     if ObjectExists( Database, Database[i] ) then
+        FindObject := @Database[i]
+     else
+        FindObject := nil;
+
+End; { End of FindObject }
 
 function ModifyObject( obj:TPerson ):TPerson; // Function returns modified object
 Begin
@@ -50,10 +71,15 @@ End; { End of ModifyObject }
 { Procedures }
 procedure InitializeDatabase( var Database:array of TPerson ); // Before you start working on database, it has to be empty
 var
-   i:integer;
+   i,id:integer;
 Begin
+     id := 1;
      for i := LOW to HIGH-1 do
-         Database[i].state := false;
+     begin
+          Database[i].id := id;
+          Database[i].state := false;
+          id := id + 1;
+     end;
 End; { End of InitializeDatabase }
 
 procedure AddObject( var Database:array of TPerson ); // Procedure adds new record to database
@@ -79,7 +105,7 @@ End; { End of ShowDatabase }
 
 procedure SortDatabase( var Database:array of TPerson; order:boolean; by:byte ); // Procedure has to sort database
 Begin // true = asc, false = desc || 0 = by ID, 1 = by name
-      writeln(order,by);
+      writeln(order, by);
 End; { End of SortDatabase }
 
 
