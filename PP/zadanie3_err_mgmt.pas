06acb1c7defeee832b0689f18a2857c360db1093
hexophile <hexophile@gmail.com>
2015-11-26 04:00:52 +0100

    Create zadanie3_err_mgmt.pas

---
+++ PP/zadanie3_err_mgmt.pas
@@ -0,0 +1,15 @@
+unit zadanie3_err_mgmt;
+
+interface
+Type
+    ErrorType = ( _file );
+    TError = record
+        errorMsg:string;
+        errorType:string;
+    End;
+
+implementation
+
+begin
+
+end.
