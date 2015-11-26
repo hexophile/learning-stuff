a98dfb4fde70c761ec471a5a92ba0d604706bbda
hexophile <hexophile@gmail.com>
2015-11-26 03:47:40 +0100

    Update zadanie3_unit.pas
    Dev-pascal sometimes is pretty retarded if it's about tab indents :(

--- PP/zadanie3_unit.pas
+++ PP/zadanie3_unit.pas
@@ -4,22 +4,25 @@ interface
  
 { Constants }
 const
-    LOW = 1;        // Starting number in database
-	HIGH = 100000;  // Ending number
-                    // LOW and HIGH are numbers that define primary key of database
+	LOW = 1;	// Starting number in database
+	HIGH = 100000;	// Ending number
+			// LOW and HIGH are numbers that define primary key of database
+	TSTATE_FALSE = false;
 { Structures }
 type
+    TState = boolean;
+
 	TPerson = record
 		id:integer;     // ID number
 		name:string;    // Main information
-		state:boolean;  // Information whether record exists
+		state:TState;  // Information whether record exists
 	End; // Main database object structure   
 	TPersonPtr = ^TPerson;
 
 	TDatabase = array[LOW..HIGH] of TPerson;
 
 { Functions }
-function ObjectExists( var Database:array of TPerson; dbObject:TPerson ):boolean;
+function ObjectState( var Database:array of TPerson; id:integer ):TState;
 function FindObject( var Database:array of TPerson; name:string ):TPersonPtr;
 function ModifyObject( obj:TPerson ):TPerson;
 
@@ -62,16 +65,16 @@ var
 Begin
 	for i := LOW to HIGH do
 	begin
-        if not Database[i].state then
-        begin
-            next := FindNextExistent( Database, i );
-            if( next = LOW-1 ) then
-            begin
-                writeln('End of data.');
-            end
-            else
-                Swap( Database[i], Database[next] );
-        end;
+		if not Database[i].state then
+		begin
+			next := FindNextExistent( Database, i );
+			if( next = LOW-1 ) then
+			begin
+				writeln('End of data.');
+		end
+		else
+			Swap( Database[i], Database[next] );
+		end;
 	end;
 End; { End of ClearEmptyRecords }
 
@@ -83,15 +86,21 @@ Begin
 
 	for i := LOW to HIGH do
 	begin
-		if( Database[i].state ) then FindRealHighValue := i;
+		if( not Database[i].state ) then FindRealHighValue := i - 1;
 	end;
+
+    if( i = HIGH ) then
+        FindRealHighValue := -1;
+{
+    Need error management,
+}
 End; { End of FindRealHighValue }
 { End of internal functions and procedures }
 
 { Export functions }
-function ObjectExists( var Database:array of TPerson; dbObject:TPerson ):boolean; // Function returns if object exists
+function ObjectState( var Database:array of TPerson; id:integer ):TState; // Function returns if object exists
 Begin
-	ObjectExists := dbObject.state;
+	ObjectState := Database[id].state;
 End; { End of ObjectExists }
 
 function FindObject( var Database:array of TPerson; name:string ):TPersonPtr; // Function has to find record and return pointer to it
@@ -111,16 +120,31 @@ Begin
 		i := i + 1;
 	end;
 
-	if ObjectExists( Database, Database[i] ) then // Yeah, no comment, poor as hell
-		FindObject := @Database[i]
+	if( ObjectState( Database, i ) = TSTATE_FALSE ) then // Yeah, no comment, poor as hell
+		FindObject := nil
 	else
-		FindObject := nil;
-
+		FindObject := @Database[i];
+{
+    TODO in extended:
+    binary tree of searching with DSW algorithm(?)
+}
 End; { End of FindObject }
 
 function ModifyObject( obj:TPerson ):TPerson; // Function returns modified object
 Begin
+    writeln( 'Old id: ':1, obj.id );
+    write( 'New id: ':1 );
+    readln( obj.id );
+    writeln( 'Old name: ':1, obj.name );
+    write( 'New name: ':1 );
+    readln( obj.name );
+
 	ModifyObject := obj;
+{
+    TODO in extended:
+    multiple modification, independence from definition of object,
+    secure forms
+}
 End; { End of ModifyObject }
 
 { Export procedures }
@@ -135,12 +159,27 @@ Begin
 		Database[i].state := false;
 		id := id + 1;
 	end;
-	Database[LOW].name := #0'Control record';
+	Database[LOW].name := #0'Table name';
 	Database[LOW].state := True;
 End; { End of InitializeDatabase }
 
 procedure AddObject( var Database:array of TPerson ); // Procedure adds new record to database
+var
+    i:integer;
 Begin
+    i := FindRealHighValue( Database );
+    if( i > 0 ) then
+    begin
+        write( 'New id: ':1 );
+        readln( Database[i+1].id );
+        write( 'New name: ':1 );
+        readln( Database[i+1].name );
+    end
+    else if( i = -1 ) then
+    begin
+        writeln('Database is full.');
+        readln;
+    end;
 End; { End of AddObject }
 
 procedure RemoveObject( var Database:array of TPerson; tabID:integer ); // Procedure removes object from database
@@ -153,18 +192,27 @@ var
 	i:integer;
 Begin
 	i := LOW;
-	while( (i <> HIGH) and Database[i].state ) do
+	while( ( i <> HIGH ) and Database[i].state ) do
 	begin
-		writeln('':1, Database[i].id, ' ', Database[i].name );
+		writeln( '':1, Database[i].id, ' ', Database[i].name );
 		i := i - 1;
 	end;
 End; { End of ShowDatabase }
 
 procedure SortDatabase( var Database:array of TPerson; order:boolean; by:byte ); // Procedure has to sort database
 Begin // true = asc, false = desc || 0 = by ID, 1 = by name
-	writeln(order, by); // TODO, simply quicksort
+	writeln( order, by ); // TODO, simply quicksort
+{
+    TODO in extended:
+    introsort
+}
 End; { End of SortDatabase }
 
 Begin
 
 End.
+{
+    TODO in extended:
+    error management unit,
+    sort state,
+}
